b9e1a90ada417b892743056342ba989eec4e76b8
Carter <cartersteinhoff@gmail.com>
2023-01-04 09:17:04 -0700

    updated products

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,6 @@
 import { getNextStaticProps } from "@faustwp/core";
 import Image from "next/image";
+import Link from "next/link";
 import { gql, useQuery } from "@apollo/client";
 
 export default function Posts() {
@@ -14,32 +15,34 @@ export default function Posts() {
             ? data.posts.nodes.map((post) => {
                 console.log(post);
                 return (
-                  <div className="my-10">
-                    <div className="flex items-center mb-10">
-                      {post.featuredImage ? (
-                        <div className="w-[400px]">
-                          <Image
-                            width="800"
-                            height="600"
-                            src={post.featuredImage.node.mediaItemUrl}
+                  <Link href={`/${encodeURIComponent(post.slug)}`}>
+                    <div className="my-10 cursor-pointer">
+                      <div className="flex items-center mb-10">
+                        {post.featuredImage ? (
+                          <div className="w-[400px]">
+                            <Image
+                              width="800"
+                              height="600"
+                              src={post.featuredImage.node.mediaItemUrl}
+                            />
+                          </div>
+                        ) : null}
+                        <div className="my-8 ml-6">
+                          <h2 className="text-2xl mb-4 font-medium">
+                            {post.title}
+                          </h2>
+                          <div
+                            className="mb-4 text-lg"
+                            dangerouslySetInnerHTML={{ __html: post.excerpt }}
                           />
+                          <p className="mb-8">
+                            {new Date(post.date).toLocaleDateString()}
+                          </p>
                         </div>
-                      ) : null}
-                      <div className="my-8 ml-6">
-                        <h2 className="text-2xl mb-4 font-medium">
-                          {post.title}
-                        </h2>
-                        <div
-                          className="mb-4 text-lg"
-                          dangerouslySetInnerHTML={{ __html: post.excerpt }}
-                        />
-                        <p className="mb-8">
-                          {new Date(post.date).toLocaleDateString()}
-                        </p>
                       </div>
+                      <div className="w-full h-[1px] bg-gray-300"></div>
                     </div>
-                    <div className="w-full h-[1px] bg-gray-300"></div>
-                  </div>
+                  </Link>
                 );
               })
             : null}
@@ -56,6 +59,7 @@ Posts.query = gql`
         date
         title
         excerpt
+        slug
         featuredImage {
           node {
             id
