3453cf7e048379ac08d2826d3fec89c6ee60ce0c
Kevin Lopez <kevinlopez@Kevins-MacBook-Air-2.local>
2023-01-19 15:35:59 -0800

    improved modal css

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -17,17 +17,26 @@ const Modal = ({ text, externalLink }) => {
         {text}
       </button>
       {isOpen && (
-        <div className="fixed top-[10%] left-[35%] w-1/3 h-52">
-          <div className="bg-slate-400 p-4">
-            <p>
+        <div className="fixed top-[10%] left-[35%] w-1/3 h-52 ">
+          <div className="bg-slate-400 p-4 rounded-md">
+            <button onClick={closeModal} className="">
+              &times;
+            </button>
+            <p className="text-lg">
               You are now leaving provepharmusa.com. Provepharm is not
               responsible for the content on any third-party website.
             </p>
-            <section className="flex justify-end">
-              <a className="p-2 bg-blue-500" href={externalLink}>
+            <section className="flex justify-end mt-4">
+              <a
+                className="mr-2 p-2 bg-blue-500 text-white font-semibold tracking-wide"
+                href={externalLink}
+              >
                 CONTINUE
               </a>
-              <button className="p-2 bg-zinc-50" onClick={closeModal}>
+              <button
+                className="p-2 bg-slate-800 text-white font-semibold tracking-wide"
+                onClick={closeModal}
+              >
                 CANCEL
               </button>
             </section>
