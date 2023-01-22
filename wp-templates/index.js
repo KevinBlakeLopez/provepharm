5cc567241513506bbacf623665b959285eb3b02f
Carter <cartersteinhoff@gmail.com>
2023-01-22 14:25:35 -0700

    updated

--- wp-templates/index.js
+++ wp-templates/index.js
@@ -1,13 +1,15 @@
-import category from './category';
-import tag from './tag';
-import frontPage from './front-page';
-import page from './page';
-import single from './single';
+import category from "./category";
+import tag from "./tag";
+import frontPage from "./front-page";
+import page from "./page";
+import single from "./single";
+import singleProducts from "./single-products";
 
 export default {
   category,
   tag,
-  'front-page': frontPage,
+  "front-page": frontPage,
   page,
   single,
+  "single-products": singleProducts,
 };
