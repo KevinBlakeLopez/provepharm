fd83573468acd31a1991dd2cef2e9a5d1f1dc9b2
Carter <cartersteinhoff@gmail.com>
2023-01-26 15:09:54 -0700

    update

--- components/ISI.js
+++ components/ISI.js
@@ -7,14 +7,21 @@ export default function ISI({ importantsafetyinformation }) {
     <section
       className={`bg-slate-400 px-2 fixed ${
         expanded ? "top-[75px]" : ""
-      } bottom-[-45px] left-0 right-0 w-full z-50 transition-[top 1s ease-in-out]`}
+      } bottom-[-128px] md:bottom-[-45px] left-0 right-0 w-full z-50 transition-transform-[top 1s ease-in-out]`}
     >
-      <div className="">
+      <div className="flex justify-between">
         <button
           className="text-center text-xl font-bold py-4 px-2"
           onClick={() => setExpanded(!expanded)}
         >
           {expanded ? "Close" : "Expand"}
+        </button>
+        <button
+          className="text-center text-xl font-bold py-4 px-2"
+          onClick={() => window.print()}
+          //   https://www.npmjs.com/package/react-to-print
+        >
+          Print
         </button>
       </div>
       <h5 className="text-center text-xl font-bold pb-6">
@@ -22,13 +29,13 @@ export default function ISI({ importantsafetyinformation }) {
       </h5>
       <div className="flex justify-center">
         <div
-          className={`isi mb-20 md:mb-0 max-w-[1400px] ${
-            expanded ? "h-[83vh]" : "h-44"
-          } overflow-auto md:pr-6`}
+          style={{ height: expanded ? "calc(100vh - 220px)" : "200px" }}
+          className={`isi mb-20 md:mb-0 max-w-[1400px] overflow-auto md:pr-6`}
           dangerouslySetInnerHTML={{
             __html: importantsafetyinformation,
           }}
         />
+        <div className="h-20"></div>
       </div>
     </section>
   );
---
+++ pages/press-releases/[slug].js
@@ -0,0 +1,6 @@
+import { getWordPressProps } from "@faustwp/core";
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+import { Header, Footer, NavigationMenu } from "../../components";
+import Container from "../../components/Container";
+import Banner from "../../components/Banner";
