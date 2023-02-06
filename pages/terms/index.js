5af19294bc5e2afcbaea0a57f2ac317e590a57b4
Kevin Lopez <kevinblakelopez@gmail.com>
2023-02-05 21:20:43 -0800

    added styles to terms page

--- pages/terms/index.js
+++ pages/terms/index.js
@@ -16,7 +16,7 @@ export default function TermsAndConditions() {
     <>
       <Header menuItems={data.headerMenuItems} />
       <Container>
-        <div>
+        <div className={styles.termsDiv}>
           <h2 className="text-3xl font-bold text-center mb-4">
             Website Terms of Use
           </h2>
