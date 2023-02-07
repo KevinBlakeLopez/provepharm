2cedf963db5ace1fafd676f52cbb3716566291b1
Kevin Lopez <kevinblakelopez@gmail.com>
2023-02-07 09:23:08 -0800

    added functionality all products page to group products of same category together

--- pages/products/index.js
+++ pages/products/index.js
@@ -14,6 +14,21 @@ export default function Products() {
     return <></>;
   }
 
+  const categories = new Set();
+  const groupedProducts = {};
+
+  data.products.nodes.forEach((product) => {
+    const category = product.categories.nodes[0]
+      ? product.categories.nodes[0].name
+      : "";
+    if (!categories.has(category)) {
+      categories.add(category);
+      groupedProducts[category] = [];
+    }
+
+    groupedProducts[category].push(product);
+  });
+
   return (
     <>
       <Header menuItems={data.headerMenuItems} />
@@ -24,91 +39,90 @@ export default function Products() {
             {/* upgrades - either make the ProductsFinder component work and/or add filter/sort functionality */}
             {/* <ProductsFinder /> */}
             {data ? (
-              data.products.nodes.map((product) => {
+              Object.keys(groupedProducts).map((category) => {
                 return (
                   <div className="my-[80px]">
                     <h2 className="text-[1.7rem] tracking-wide mb-8">
-                      {product.metaFields.brandProduct
-                        ? `${product.metaFields.productvariationtitle} `
-                        : ""}{" "}
-                      {product.metaFields.genericname}
+                      {category}
                     </h2>
-                    <div className="md:flex md:justify-between">
-                      {product.metaFields.productimage ? (
-                        <Image
-                          src={product.metaFields.productimage.mediaItemUrl}
-                          width="130"
-                          height="250"
-                        />
-                      ) : (
-                        <div className="h-[250px] w-[140px] bg-primary flex justify-center items-center text-center">
-                          <p className="text-white text-2xl font-medium p-2">
-                            Photo Coming Soon
-                          </p>
+                    {groupedProducts[category].map((product) => (
+                      <div className="md:flex md:justify-between mb-16">
+                        {product.metaFields.productimage ? (
+                          <Image
+                            src={product.metaFields.productimage.mediaItemUrl}
+                            width="130"
+                            height="250"
+                          />
+                        ) : (
+                          <div className="h-[250px] w-[140px] bg-primary flex justify-center items-center text-center">
+                            <p className="text-white text-2xl font-medium p-2">
+                              Photo Coming Soon
+                            </p>
+                          </div>
+                        )}
+                        <div className="flex">
+                          <ul className="mr-8 w-[210px]">
+                            <li className="font-medium text-lg text-[#1e387d] ">
+                              {product.metaFields.brandProduct
+                                ? "Brand Name:"
+                                : "Reference Listed Drug:"}
+                            </li>
+                            <li className="mb-4">
+                              {product.metaFields.productvariationtitle}
+                            </li>
+                            <li className="font-medium text-lg text-[#1e387d]">
+                              Form:
+                            </li>
+                            <li className="mb-4">{product.metaFields.form}</li>
+                            <li className="font-medium text-lg text-[#1e387d]">
+                              Strength:
+                            </li>
+                            <li className="mb-4">
+                              {product.metaFields.strength}
+                            </li>
+                            <li className="mt-5 bg-[#00a6e7] w-10/12 text-white">
+                              <div className="py-2 px-2 text-center">
+                                <Link
+                                  href={`/products/${encodeURIComponent(
+                                    product.slug
+                                  )}`}
+                                >
+                                  View Product
+                                </Link>
+                              </div>
+                            </li>
+                          </ul>
+                          <ul>
+                            <li className="font-medium text-lg text-[#1e387d]">
+                              Pack Size:
+                            </li>
+                            <li className="mb-4">
+                              {product.metaFields.packsize}
+                            </li>
+                            <li className="font-medium text-lg text-[#1e387d]">
+                              NDC:
+                            </li>
+                            <li className="mb-4">{product.metaFields.ndc}</li>
+                            <li className="font-medium text-lg text-[#1e387d]">
+                              Safety Data Sheet:
+                            </li>
+                            <li className="mb-4 underline underline-offset-2 text-blue-500">
+                              {product.metaFields.safetysheet ? (
+                                <a
+                                  href={
+                                    product.metaFields.safetysheet.mediaItemUrl
+                                  }
+                                >
+                                  Click Here PDF
+                                </a>
+                              ) : (
+                                <h3>Please add PDF here</h3>
+                              )}
+                            </li>
+                          </ul>
                         </div>
-                      )}
-                      <div className="flex">
-                        <ul className="mr-8 w-[210px]">
-                          <li className="font-medium text-lg text-[#1e387d] ">
-                            {product.metaFields.brandProduct
-                              ? "Brand Name:"
-                              : "Reference Listed Drug:"}
-                          </li>
-                          <li className="mb-4">
-                            {product.metaFields.productvariationtitle}
-                          </li>
-                          <li className="font-medium text-lg text-[#1e387d]">
-                            Form:
-                          </li>
-                          <li className="mb-4">{product.metaFields.form}</li>
-                          <li className="font-medium text-lg text-[#1e387d]">
-                            Strength:
-                          </li>
-                          <li className="mb-4">
-                            {product.metaFields.strength}
-                          </li>
-                          <li className="mt-5 bg-[#00a6e7] w-10/12 text-white">
-                            <div className="py-2 px-2 text-center">
-                              <Link
-                                href={`/products/${encodeURIComponent(
-                                  product.slug
-                                )}`}
-                              >
-                                View Product
-                              </Link>
-                            </div>
-                          </li>
-                        </ul>
-                        <ul>
-                          <li className="font-medium text-lg text-[#1e387d]">
-                            Pack Size:
-                          </li>
-                          <li className="mb-4">
-                            {product.metaFields.packsize}
-                          </li>
-                          <li className="font-medium text-lg text-[#1e387d]">
-                            NDC:
-                          </li>
-                          <li className="mb-4">{product.metaFields.ndc}</li>
-                          <li className="font-medium text-lg text-[#1e387d]">
-                            Safety Data Sheet:
-                          </li>
-                          <li className="mb-4 underline underline-offset-2 text-blue-500">
-                            {product.metaFields.safetysheet ? (
-                              <a
-                                href={
-                                  product.metaFields.safetysheet.mediaItemUrl
-                                }
-                              >
-                                Click Here PDF
-                              </a>
-                            ) : (
-                              <h3>Please add PDF here</h3>
-                            )}
-                          </li>
-                        </ul>
                       </div>
-                    </div>
+                    ))}
                   </div>
                 );
               })
@@ -129,6 +143,11 @@ Products.query = gql`
     products(first: 30) {
       nodes {
         title
+        categories {
+          nodes {
+            name
+          }
+        }
         metaFields {
           brandProduct
           productvariationtitle
