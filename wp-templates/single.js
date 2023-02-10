dc61b0be67bcb2ed8b45d68a2034b6d4fb224a8e
Carter <cartersteinhoff@gmail.com>
2023-02-10 14:00:11 -0700

    Fixed error

--- wp-templates/single.js
+++ wp-templates/single.js
@@ -1,106 +1,11 @@
-import { gql } from "@apollo/client";
-import * as MENUS from "../constants/menus";
-import { BlogInfoFragment } from "../fragments/GeneralSettings";
-import {
-  // Header,
-  Footer,
-  Main,
-  EntryHeader,
-  NavigationMenu,
-  ContentWrapper,
-  FeaturedImage,
-  // SEO,
-} from "../components";
-import Container from "../components/Container";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Component(props) {
-  // Loading state for previews
-  if (props.loading) {
-    return <>Loading...</>;
-  }
-
-  const { title: siteTitle, description: siteDescription } =
-    props?.data?.generalSettings;
-  const primaryMenu = props?.data?.headerMenuItems?.nodes ?? [];
-  const footerMenu = props?.data?.footerMenuItems?.nodes ?? [];
-  const { title, content, featuredImage, date, author } = props.data.post;
-
   return (
     <>
-      {/* <SEO
-        title={siteTitle}
-        description={siteDescription}
-        imageUrl={featuredImage?.node?.sourceUrl}
-      /> */}
-      {/* <Header
-        title={siteTitle}
-        description={siteDescription}
-        menuItems={primaryMenu}
-      /> */}
-      <Main>
-        <>
-          <div className="mt-20 flex justify-center">
-            <div className="w-[800px]">
-              <EntryHeader
-                title={title}
-                image={featuredImage?.node}
-                date={date}
-                author={author?.node?.name}
-              />
-              <Container>
-                <ContentWrapper content={content} />
-              </Container>
-            </div>
-          </div>
-        </>
-      </Main>
-      <Footer title={siteTitle} menuItems={footerMenu} />
+      <Header />
+      <Footer />
     </>
   );
 }
-
-Component.query = gql`
-  ${BlogInfoFragment}
-  ${NavigationMenu.fragments.entry}
-  ${FeaturedImage.fragments.entry}
-  query GetPost(
-    $databaseId: ID!
-    $headerLocation: MenuLocationEnum
-    $footerLocation: MenuLocationEnum
-    $asPreview: Boolean = true
-  ) {
-    post(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
-      title
-      content
-      date
-      author {
-        node {
-          name
-        }
-      }
-      ...FeaturedImageFragment
-    }
-    generalSettings {
-      ...BlogInfoFragment
-    }
-    headerMenuItems: menuItems(where: { location: $headerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
-      }
-    }
-    footerMenuItems: menuItems(where: { location: $footerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
-      }
-    }
-  }
-`;
-
-Component.variables = ({ databaseId }, ctx) => {
-  return {
-    databaseId,
-    headerLocation: MENUS.PRIMARY_LOCATION,
-    footerLocation: MENUS.FOOTER_LOCATION,
-    asPreview: ctx?.asPreview,
-  };
-};
