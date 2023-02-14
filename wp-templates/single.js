6a30f3c3d2dd6ff575723b1a9f9757c0b07de5b1
Carter <cartersteinhoff@gmail.com>
2023-02-13 18:20:54 -0700

    deleted pages

--- wp-templates/single.js
+++ wp-templates/single.js
@@ -1,11 +1,54 @@
+import { gql } from "@apollo/client";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Container from "../components/Container";
+import SinglePostTemplate from "../components/SinglePostTemplate";
 
-export default function Component(props) {
+export default function Post(props) {
+  console.log(props);
+  if (props.loading) {
+    return <>Loading...</>;
+  }
+  console.log(props);
+  const { post } = props.data;
+
   return (
     <>
       <Header />
+      <Container size="xxs">
+        <SinglePostTemplate data={post} />
+      </Container>
       <Footer />
     </>
   );
 }
+
+Post.query = gql`
+  query PostQuery($databaseId: ID!, $asPreview: Boolean = false) {
+    post(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
+      id
+      date
+      dateGmt
+      title
+      content
+      featuredImage {
+        node {
+          uri
+          mediaItemUrl
+          mediaDetails {
+            width
+            height
+          }
+        }
+      }
+    }
+  }
+`;
+
+Post.variables = ({ databaseId }, ctx) => {
+  return {
+    databaseId,
+    asPreview: ctx?.asPreview,
+  };
+};
