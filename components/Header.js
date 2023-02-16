047cabd0367ca55fd44ca17011eb5478ca0d6973
Carter <cartersteinhoff@gmail.com>
2023-02-16 16:45:05 -0700

    update menu

--- components/Header.js
+++ components/Header.js
@@ -32,7 +32,9 @@ export default function Header({ menuItems }) {
       />
       <header
         className={`bg-white
-         border-b-2 w-full p-5 ${isNavShown ? "fixed top-0 width:100%" : ""}`}
+         border-b-2 w-full p-5 ${
+           isNavShown ? "h-full absolute overflow-hidden top-0 width:100%" : ""
+         }`}
       >
         <div className="flex justify-between items-center">
           <div className="cursor-pointer">
@@ -52,33 +54,31 @@ export default function Header({ menuItems }) {
               className={
                 isNavShown
                   ? `
-                  fixed left-0 top-24 w-full h-full pt-5 pl-5 bg-white
-                   z-50
-                  md:relative md:top-[initial]
+                  w-full h-full overflow-hidden absolute left-0 top-24  pt-7 pl-7 bg-white z-50 md:relative md:top-[initial]
                   `
                   : "hidden md:block"
               }
             >
               <ul className="text-[#1e387d] md:flex">
-                <li className="text-lg md:text-base mb-3 md:mb-0 md:mr-4">
+                <li className="text-2xl md:text-base mb-6 md:mb-0 md:mr-4">
                   <Link href="/about">About Us</Link>
                 </li>
-                <li className="text-lg md:text-base mb-3 md:mb-0 md:mr-4">
+                <li className="text-2xl md:text-base mb-6 md:mb-0 md:mr-4">
                   <Link href="/products">Products</Link>
                 </li>
-                <li className="text-lg md:text-base mb-3 md:mb-0 md:mr-4">
+                <li className="text-2xl md:text-base mb-6 md:mb-0 md:mr-4">
                   <Link href="/bludigo">Bludigo™</Link>
                 </li>
-                <li className="text-lg md:text-base mb-3 md:mb-0 md:mr-4">
+                <li className="text-2xl md:text-base mb-6 md:mb-0 md:mr-4">
                   <Link href="/press">Press Releases</Link>
                 </li>
-                <li className="text-lg md:text-base mb-3 md:mb-0 md:mr-4">
+                <li className="text-2xl md:text-base mb-6 md:mb-0 md:mr-4">
                   <Link href="/news">News</Link>
                 </li>
-                <li className="text-lg md:text-base mb-3 md:mb-0 md:mr-4">
+                <li className="text-2xl md:text-base mb-6 md:mb-0 md:mr-4">
                   <Link href="/events">Events</Link>
                 </li>
-                <li className="text-lg md:text-base mb-3 md:mb-0 md:mr-4">
+                <li className="text-2xl md:text-base mb-6 md:mb-0 md:mr-4">
                   <Link href="/contact">Contact</Link>
                 </li>
               </ul>
