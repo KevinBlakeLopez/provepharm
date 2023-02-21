2f7f8edfdfb6827e159526514b0b8ad0a49d7190
Carter <cartersteinhoff@gmail.com>
2023-02-21 16:09:27 -0700

    update

--- components/BannerBludigo.js
+++ components/BannerBludigo.js
@@ -6,28 +6,29 @@ import Container from "./Container";
 export default function BannerBludigo() {
   return (
     <div className="w-full min-h-[400px] bg-white text-white md:flex justify-center items-center my-40">
-      <div className="w-full md:max-h-[275px] md:p-6 bg-gradient-to-b from-[#4E157D] to-[#230A2F] text-white md:flex justify-center items-center">
+      <div className="w-full md:max-h-[275px] md:p-6 bg-gradient-to-l from-[#31023d] to-primary text-white md:flex justify-center items-center">
         <Container size="md">
           <div className="flex flex-col md:flex-row items-center justify-center">
-            <div className="md:w-1/2 md:mx-20">
-              <figure>
-                <Image src={bludigoProduct} />
-              </figure>
-            </div>
-            <div className="md:w-1/2 px-2 py-10">
+          <div className="md:w-1/2 px-2 py-10">
               <p className="text-2xl mb-12 md:mb-8">
                 The first and only FDA-approved injectable indigo carmine
                 diagnostic dye.
               </p>
               <Link href="/bludigo">
-                <button className="bg-[#7200A8] px-6 py-[11px] rounded-lg text-xl">
+                <button className="border-2 border-white px-6 py-[11px] rounded-lg text-xl">
                   Explore the benefits.
                 </button>
               </Link>
             </div>
+            <div className="md:w-1/2 md:ml-20">
+              <figure>
+                <Image src={bludigoProduct} />
+              </figure>
+            </div>
+           
           </div>
         </Container>
-    </div>
+      </div>
     </div>
   );
 }
