2c111844b2b5c5c8c7fd9723fbe4574437019146
Kevin Lopez <kevinblakelopez@gmail.com>
2023-02-21 18:00:58 -0800

    added security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
 const { withFaust, getWpHostname } = require("@faustwp/core");
 
+const securityHeaders = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+];
+
 /**
  * @type {import('next').NextConfig}
  **/
@@ -14,5 +33,8 @@ module.exports = withFaust({
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  async headers() {
+    return [...securityHeaders, ...(await withFaust({}).headers())];
   },
 });
