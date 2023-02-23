19883c19108d3c5bd643efe768354579a5aec3a0
Kevin Lopez <kevinblakelopez@gmail.com>
2023-02-23 11:04:27 -0800

    test Vercel backup

--- pages/news/index.js
+++ pages/news/index.js
@@ -23,6 +23,7 @@ export default function Posts() {
       <Header />
       <Banner>News</Banner>
       <Container size="sm">
+        <h1 className="text-6xl">TEST for Vercel Backup</h1>
         <AllPostsTemplate data={data.posts} route="news" />
       </Container>
       <Footer />
